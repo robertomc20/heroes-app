3d459af806bc325acec47f408cf0bf0a6d1df6f7
robertomc20 <roberto.marihuan.crisostomo@gmail.com>
2021-04-19 18:45:36 -0400

    segundo commit

---
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap, tap } from "rxjs/operators";
+
+import { HeroesService } from '../../services/heroes.service';
+
+import { Heroe } from '../../interfaces/heroes.interface';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+    mat-list-item {
+      margin-top:10px
+    }
+  `]
+})
+export class HeroeComponent implements OnInit {
+
+  heroe!: Heroe;
+
+  constructor( 
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.heroesService.getHeroePorId( id ) ),
+        tap(console.log)
+      )
+      .subscribe( heroe => this.heroe = heroe );
+  }
+
+  regresar() {
+    this.router.navigate(['heroes/listado']);
+  }
+
+}

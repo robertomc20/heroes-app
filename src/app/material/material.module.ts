a828f2e8c7c1a257f1e0736b1d4086db557e60a0
robertomc20 <roberto.marihuan.crisostomo@gmail.com>
2021-04-20 15:08:08 -0400

    tercer commit, sección 14

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -20,12 +23,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
     MatGridListModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
     MatListModule,
+    MatSelectModule,
     MatSidenavModule,
+    MatSnackBarModule,
     MatProgressSpinnerModule,
     MatToolbarModule
   ]
